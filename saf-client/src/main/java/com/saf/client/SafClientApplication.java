9cf389e287fa020e046f9ededd8ba45ead05e681
YounesD75 <133532025+YounesD75@users.noreply.github.com>
2025-11-12 11:12:14 +0100

    Fix Maven modules and add saf-client

---
+++ saf-client/src/main/java/com/saf/client/SafClientApplication.java
@@ -0,0 +1,13 @@
+package com.saf.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafClientApplication.class, args);
+    }
+
+}
---
+++ saf-client/src/test/java/com/saf/client/SafClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saf.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

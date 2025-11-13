ac7d6433e6a85ad3a6b889aae4cf031b387ce0ef
YounesD75 <133532025+YounesD75@users.noreply.github.com>
2025-11-13 09:30:05 +0100

    problème à fixer au niveau de ActorCommand dans SAF-client pour impléments l'interface du framework

--- saf-client/src/test/java/com/saf/client/SafClientApplicationTests.java
+++ saf-client/src/test/java/com/saf/client/SafClientApplicationTests.java
@@ -2,6 +2,9 @@ package com.saf.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
 class SafClientApplicationTests {
--- saf-core1/src/test/java/com/saf/core1/Actor.java
+++ saf-core1/src/test/java/com/saf/core1/Actor.java
@@ -1,7 +1,30 @@
 package com.saf.core1;
 
+/**
+ * Interface de base pour tout acteur du framework SAF.
+ * Un acteur définit un comportement asynchrone : il reçoit des messages et réagit.
+ */
 public interface Actor {
-    default void onStart() {}                                   // appelé à la création
-    void onReceive(ActorContext ctx, Message msg) throws Exception; // appelé à chaque message reçu
-    default void onStop() {}                                    // appelé à la destruction
+
+    /**
+     * Méthode appelée automatiquement à la création de l’acteur.
+     * Peut être utilisée pour initialiser des ressources.
+     */
+    default void onStart() {}
+
+    /**
+     * Méthode principale de l’acteur.
+     * Appelée à chaque fois qu’un message est reçu dans sa mailbox.
+     *
+     * @param ctx le contexte d’exécution de l’acteur (pour accéder à self(), spawn(), stop(), etc.)
+     * @param msg le message reçu
+     * @throws Exception si une erreur se produit lors du traitement
+     */
+    void onReceive(ActorContext ctx, Message msg) throws Exception;
+
+    /**
+     * Méthode appelée automatiquement avant la destruction de l’acteur.
+     * Peut être utilisée pour fermer des ressources ou sauvegarder un état.
+     */
+    default void onStop() {}
 }
